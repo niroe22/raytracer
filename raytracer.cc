9514f7e9f47ea9c4801b3886acec8b28b006219d
nkls <niki.roemer@gmail.com>
2024-04-02 16:25:39 +0200

    Initial commit

---
+++ raytracer.cc
@@ -0,0 +1,99 @@
+#include "math.h"
+#include "geometry.h"
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+// Die folgenden Kommentare beschreiben Datenstrukturen und Funktionen
+// Die Datenstrukturen und Funktionen die weiter hinten im Text beschrieben sind,
+// hängen höchstens von den vorhergehenden Datenstrukturen ab, aber nicht umgekehrt.
+
+
+
+// Ein "Bildschirm", der das Setzen eines Pixels kapselt
+// Der Bildschirm hat eine Auflösung (Breite x Höhe)
+// Kann zur Ausgabe einer PPM-Datei verwendet werden oder
+// mit SDL2 implementiert werden.
+
+
+
+// Eine "Kamera", die von einem Augenpunkt aus in eine Richtung senkrecht auf ein Rechteck (das Bild) zeigt.
+// Für das Rechteck muss die Auflösung oder alternativ die Pixelbreite und -höhe bekannt sein.
+// Für ein Pixel mit Bildkoordinate kann ein Sehstrahl erzeugt werden.
+
+
+
+// Für die "Farbe" benötigt man nicht unbedingt eine eigene Datenstruktur.
+// Sie kann als Vector3df implementiert werden mit Farbanteil von 0 bis 1.
+// Vor Setzen eines Pixels auf eine bestimmte Farbe (z.B. 8-Bit-Farbtiefe),
+// kann der Farbanteil mit 255 multipliziert  und der Nachkommaanteil verworfen werden.
+
+
+// Das "Material" der Objektoberfläche mit ambienten, diffusem und reflektiven Farbanteil.
+
+
+
+// Ein "Objekt", z.B. eine Kugel oder ein Dreieck, und dem zugehörigen Material der Oberfläche.
+// Im Prinzip ein Wrapper-Objekt, das mindestens Material und geometrisches Objekt zusammenfasst.
+// Kugel und Dreieck finden Sie in geometry.h/tcc
+
+
+// verschiedene Materialdefinition, z.B. Mattes Schwarz, Mattes Rot, Reflektierendes Weiss, ...
+// im wesentlichen Variablen, die mit Konstruktoraufrufen initialisiert werden.
+
+
+// Die folgenden Werte zur konkreten Objekten, Lichtquellen und Funktionen, wie Lambertian-Shading
+// oder die Suche nach einem Sehstrahl für das dem Augenpunkt am nächsten liegenden Objekte,
+// können auch zusammen in eine Datenstruktur für die gesammte zu
+// rendernde "Szene" zusammengefasst werden.
+
+// Die Cornelbox aufgebaut aus den Objekten
+// Am besten verwendet man hier einen std::vector< ... > von Objekten.
+
+// Punktförmige "Lichtquellen" können einfach als Vector3df implementiert werden mit weisser Farbe,
+// bei farbigen Lichtquellen müssen die entsprechenden Daten in Objekt zusammengefaßt werden
+// Bei mehreren Lichtquellen können diese in einen std::vector gespeichert werden.
+
+// Sie benötigen eine Implementierung von Lambertian-Shading, z.B. als Funktion
+// Benötigte Werte können als Parameter übergeben werden, oder wenn diese Funktion eine Objektmethode eines
+// Szene-Objekts ist, dann kann auf die Werte teilweise direkt zugegriffen werden.
+// Bei mehreren Lichtquellen muss der resultierende diffuse Farbanteil durch die Anzahl Lichtquellen geteilt werden.
+
+// Für einen Sehstrahl aus allen Objekte, dasjenige finden, das dem Augenpunkt am nächsten liegt.
+// Am besten einen Zeiger auf das Objekt zurückgeben. Wenn dieser nullptr ist, dann gibt es kein sichtbares Objekt.
+
+// Die rekursive raytracing-Methode. Am besten ab einer bestimmten Rekursionstiefe (z.B. als Parameter übergeben) abbrechen.
+
+
+int main(void) {
+    // Bildschirm erstellen
+    // Kamera erstellen
+    // Für jede Pixelkoordinate x,y
+    //   Sehstrahl für x,y mit Kamera erzeugen
+    //   Farbe mit raytracing-Methode bestimmen
+    //   Beim Bildschirm die Farbe für Pixel x,y, setzten
+
+    //Image
+    int image_width = 256;
+    int image_height = 256;
+    //Render
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+    for (int j = 0; j < image_height; ++j) {
+        std::clog<<"\r Scanline remaining: "<<(image_height - j) << ' ' <<std::flush;
+        for (int i = 0; i < image_width; ++i) {
+            auto r = double(i) / (image_width - 1);
+            auto g = double(j) / (image_height - 1);
+            auto b = 0;
+
+            int ir = static_cast<int>(255.999 * r);
+            int ig = static_cast<int>(255.999 * g);
+            int ib = static_cast<int>(255.999 * b);
+
+            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+        }
+    }
+
+    std::clog <<"\rDone.                \n";
+    return 0;
+}
+
